6c68f7a3b1e46cd12694fe4fcc7ba4ee364767aa
Roberta Jorgo <robertajorgo@gmail.com>
2020-12-15 21:04:42 +0100

    Initial Commit

---
+++ TestingAlamo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestingAlamo
+//
+//  Created by Roberta Jorgo on 15.12.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestingAlamo/TestingAlamoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingAlamoApp.swift
+//  TestingAlamo
+//
+//  Created by Roberta Jorgo on 15.12.20.
+//
+
+import SwiftUI
+
+@main
+struct TestingAlamoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
